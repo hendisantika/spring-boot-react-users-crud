7538a94d5454d8c9bbe567d18f7d1b12aa60a6aa
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 06:59:11 +0700

    Create Spring Boot App🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/userscrud/SpringBootReactUsersCrudApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.userscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactUsersCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactUsersCrudApplication.class, args);
+    }
+
+}
