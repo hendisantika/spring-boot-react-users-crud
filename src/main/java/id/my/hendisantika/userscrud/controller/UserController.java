704ba2f9a561dac6e7f91167ce3da760855f6664
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:07:22 +0700

    Add updateUser in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
+++ src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,5 +48,15 @@ public class UserController {
     public User getUserById(@PathVariable Long id) {
         return userRepository.findById(id)
                 .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id).map(user -> {
+            user.setUsername(newUser.getUsername());
+            user.setName(newUser.getName());
+            user.setEmail(newUser.getEmail());
+            return userRepository.save((user));
+        }).orElseThrow(() -> new UserNotFoundException(id));
     }
 }
