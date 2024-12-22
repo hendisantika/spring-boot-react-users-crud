85deb94e59f9a07ca2dbc76afd6f215cd8243c5a
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:05:58 +0700

    Add UserController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.userscrud.controller;
+
+import id.my.hendisantika.userscrud.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-users-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/12/24
+ * Time: 07.05
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin("http://localhost:3000")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserRepository userRepository;
+}
