993f5d55db68ff174c6bf090e6669548e8a246e6
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:06:46 +0700

    Add getAllUsers in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
+++ src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
@@ -5,10 +5,13 @@ import id.my.hendisantika.userscrud.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -31,5 +34,10 @@ public class UserController {
     @PostMapping
     public User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
     }
 }
