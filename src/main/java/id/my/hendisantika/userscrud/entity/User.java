e88b518cafa9ea5bf0cd62c67fdfede4889f7796
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:02:46 +0700

    Add User Entity🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/userscrud/entity/User.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.userscrud.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-users-crud
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 22/12/24
+ * Time: 07.02
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String name;
+    private String email;
+}
