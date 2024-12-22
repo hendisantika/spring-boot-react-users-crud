8c0ef77047f99aa84d69ee33f67ba8781b2a3862
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:06:26 +0700

    Add newUser in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
+++ src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
@@ -1,9 +1,12 @@
 package id.my.hendisantika.userscrud.controller;
 
+import id.my.hendisantika.userscrud.entity.User;
 import id.my.hendisantika.userscrud.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,4 +27,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserRepository userRepository;
+
+    @PostMapping
+    public User newUser(@RequestBody User newUser) {
+        return userRepository.save(newUser);
+    }
 }
