543e852b0166cc130bdc0bfc52d8fc06adab4090
hendisantika <hendisantika@yahoo.co.id>
2024-12-22 07:07:04 +0700

    Add getUserById in UserController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
+++ src/main/java/id/my/hendisantika/userscrud/controller/UserController.java
@@ -1,11 +1,13 @@
 package id.my.hendisantika.userscrud.controller;
 
 import id.my.hendisantika.userscrud.entity.User;
+import id.my.hendisantika.userscrud.exception.UserNotFoundException;
 import id.my.hendisantika.userscrud.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,5 +41,11 @@ public class UserController {
     @GetMapping
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new UserNotFoundException(id));
     }
 }
